a26c8645d6f068b91e4e40ea4c69ad8b6173e838
mertcanakdas <mertcan.akdas@gmail.com>
2024-02-10 14:21:39 +0300

    Website Deneme

---
+++ Website Deneme/app.js
@@ -0,0 +1,7 @@
+//! odd(tek) - even(çift) css selector ile de seçilebilir
+// const menuElements = document.querySelectorAll("li:nth-child(odd)");
+
+// menuElements.forEach(function(element){
+//     element.style.backgroundColor = "red";
+// });
+// console.log(menuElements);
